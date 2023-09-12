c9397016f211e919925f5d9566ce41de43fd459a
Iker Ocio <iker.ocio@bystronic.com>
2023-09-12 17:29:59 +0200

    Favicon updated

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -30,31 +30,39 @@ const PokemonSearch: React.FC = () => {
   }
 
   return (
-    <div className="p-4">
-      <input
-        type="text"
-        placeholder="Search for a Pokemon..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className="p-2 border rounded"
-      />
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-800 mb-4">Pokédex</h1>
+      <div className="w-full sm:max-w-screen-xl mb-4">
+        <input
+          type="text"
+          placeholder="Search for a Pokémon..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="p-2 border rounded w-full"
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
         {filteredPokemons.length === 0 ? (
-          <div className="col-span-3 text-center text-gray-600">
+          <div className="col-span-full text-center text-gray-600">
             No Pokémon found.
           </div>
         ) : (
           filteredPokemons.map((pokemon, index) => (
-            <div key={index} data-id={index} className="border p-2">
+            <div
+              key={index}
+              data-id={index}
+              className="border p-2 relative hover:transform hover:scale-105 transition-transform">
+              <p className="absolute top-2 left-2 text-gray-500 font-semibold">
+                {index + 1}
+              </p>
               <img
                 src={(pokemon.sprites && pokemon.sprites.front_default) || "#"}
                 alt={pokemon.name}
-                className="w-full"
+                className="w-full max-h-60 h-auto"
               />
               <Link
                 to={`/pokemon/${pokemon.name}`}
-                className="text-center mt-2 block"
-              >
+                className="text-center mt-2 block text-blue-800 font-semibold">
                 {pokemon.name}
               </Link>
             </div>
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,13 +3,37 @@ import { Pokemon } from "../types"; // Asumiendo que tienes un tipo llamado 'Pok
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 export const fetchPokemons = async (): Promise<Pokemon[]> => {
-  const response = await fetch(`${BASE_URL}/pokemon?limit=100`); // Limitado a 100 para el ejemplo, ajusta según sea necesario
+  const response = await fetch(`${BASE_URL}/pokemon?limit=151`); // Limitado a 100 para el ejemplo, ajusta según sea necesario
   const data = await response.json();
-  return data.results; // La API retorna la lista en la clave 'results'
+  //return data.results; // La API retorna la lista en la clave 'results'
+  const formattedData = await Promise.all(
+    data.results.map(async (pokemon: any) => {
+      const pokemonDetail = await fetchPokemonByName(pokemon.name);
+      return pokemonDetail;
+    })
+  );
+  console.log(formattedData);
+  return formattedData;
 };
 
 export const fetchPokemonByName = async (name: string): Promise<Pokemon> => {
+  function capitalizeFirstLetter(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   const response = await fetch(`${BASE_URL}/pokemon/${name}`);
   const data = await response.json();
-  return data;
+  const sprites = {
+    front_default: data.sprites.other["official-artwork"].front_default,
+  };
+  return {
+    id: parseInt(data.id),
+    name: capitalizeFirstLetter(data.name),
+    sprites: sprites,
+    species: {
+      name: data.name,
+      url: data.url,
+    },
+    evolutions: [], // Puedes llenar esto si tienes datos de evolución
+  };
 };
